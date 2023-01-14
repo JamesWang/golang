0bebafce960e5310cdd3549b7e4c4043c1052ec8
DESKTOP-84BFPQT\james <james.wang.w@gmail.com>
2023-01-14 11:56:50 -0500

    testing

--- requestRouting/tests/simple_test.go
+++ requestRouting/tests/simple_test.go
@@ -1,13 +1,49 @@
 package tests
 
-import "testing"
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+type SumTest struct {
+	testValues     []int
+	expectedResult int
+}
 
 func TestSum(t *testing.T) {
-	testValues := []int{10, 20, 30}
-	_, sum := SortAndTotal(testValues)
-	expected := 60
+	testVals := []SumTest{
+		{testValues: []int{10, 20, 30}, expectedResult: 60},
+		{testValues: []int{-10, 0, -10}, expectedResult: -20},
+		{testValues: []int{-10, 0, -10}, expectedResult: -20},
+	}
 
-	if sum != expected {
-		t.Fatalf("Expected %v, Got %v", expected, sum)
+	for index, testVal := range testVals {
+		t.Run(fmt.Sprintf("Sum #%v", index), func(subT *testing.T) {
+			if t.Failed() {
+				subT.SkipNow()
+			}
+			_, sum := SortAndTotal(testVal.testValues)
+			if sum != testVal.expectedResult {
+				t.Fatalf("Expected %v, Got %v", testVal.expectedResult, sum)
+			}
+		})
+	}
+}
+
+func TestSort(t *testing.T) {
+	slices := [][]int{
+		{1, 279, 48, 12, 3},
+		{-10, 0, -10},
+		{1, 2, 3, 4, 5, 6, 7},
+		{1},
+	}
+	for index, data := range slices {
+		t.Run(fmt.Sprintf("Sort #%v", index), func(subT *testing.T) {
+			sorted, _ := SortAndTotal(data)
+			if !sort.IntsAreSorted(sorted) {
+				subT.Fatalf("Unsorted data %v", sorted)
+			}
+		})
 	}
 }
